426522685e8f5cc4d1a79ae4efa8f4cf81ac2696
Guilherme Cordeiro <guilherme.cordeiro@valecard.com.br>
2025-02-25 17:02:01 -0300

    correção de erro java localdate

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,6 +31,9 @@ dependencies {
 	implementation("io.jsonwebtoken:jjwt-api:0.11.5")
 	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
 	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
+	implementation("com.nimbusds:nimbus-jose-jwt:9.37.3") {
+		exclude(group = "com.google.code.gson", module = "gson")
+	}
 	compileOnly("org.projectlombok:lombok")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	annotationProcessor("org.projectlombok:lombok")
